4338615134432f084ff8e4d7b6af471dd5530de2
Russal Arya <russalarya@gmail.com>
2026-08-13 20:37:44 +1000

    Connect Firebase Storage to Admin SDK

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
+import { getStorage } from "firebase-admin/storage";
 
 const adminApp =
   getApps().length === 0
@@ -10,10 +11,12 @@ const adminApp =
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         }),
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       })
     : getApps()[0];
 
 const adminDb = getFirestore(adminApp);
 const adminAuth = getAuth(adminApp);
+const adminStorage = getStorage(adminApp);
 
-export { adminDb, adminAuth };
+export { adminDb, adminAuth, adminStorage };
